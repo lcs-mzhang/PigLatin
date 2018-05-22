435a5379f091240db5ed9e1689425b561590109c
Zhang Matt <mzhang@lcs.on.ca>
2018-05-22 00:19:16 -0400

    Finally done.
    Thanks for the red herring about reversed strings, Mr Gordon.

--- PigLatin/main.swift
+++ PigLatin/main.swift
@@ -7,33 +7,61 @@ import Foundation
 
 // INPUT
 // Global variable, tracks how many words we can expect to have to translate to Pig Latin
-var countOfExpectedWordsToTranslate = 3
-
-// Write a loop to actually collect the expected count of words to be translated from user
-// e.g.: write the rest of the INPUT section
-
+var expectedWords = 0
+while true {
+    print("How many words will be provided?")
+    guard let givenInput = readLine() else {
+        continue
+    }
+    guard let integerInput = Int(givenInput) else {
+        continue
+    }
+    if integerInput < 0 || integerInput > 10 {
+        continue
+    }
+    expectedWords = integerInput
+    break
+}
 
 // PROCESS & OUTPUT
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 
 // Collect the words to be translated
-for counter in 1...countOfExpectedWordsToTranslate {
+for counter in 1...expectedWords {
     
-    // Ask user for the word to be translated
+    var translated = ""
+    var firstVowel = ""
+    var beforeVowel = ""
+    var afterVowel = ""
+    
     print("Enter word #\(counter):")
-    
-    // Create a variable that has the translated word
-    var translatedWord = ""
-    
-    // Get the input (use guard-let to guarantee it is not nil)
-    // and then print it out
     guard let givenInput = readLine() else {
-        // If someone enters nil input, just skip to the next line
         continue
     }
+
+    for character in givenInput {
+        switch character {
+        case "A", "E", "I", "O", "U", "Y":
+            if firstVowel == "" {
+            firstVowel += String(character)
+            } else {
+                afterVowel += String(character)
+            }
+            break
+        default:
+            if firstVowel == "" {
+                beforeVowel += String(character)
+            } else {
+                afterVowel += String(character)
+            }
+        }
+    }
     
-    // Replace this logic with the correct logic
-    translatedWord = givenInput
+    translated += firstVowel
+    translated += afterVowel
+    translated += beforeVowel
+    translated += "AY"
     
+    print("In pig latin: \(translated)")
 }
